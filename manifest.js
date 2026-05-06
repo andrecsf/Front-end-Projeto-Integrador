aea95abd4b4b8c956895a48dd13b766c7c8cfae3
andrecsf <123967995+andrecsf@users.noreply.github.com>
2026-05-06 08:51:07 -0300

    Create manifest.js

---
+++ manifest.js
@@ -0,0 +1,37 @@
+{
+  "name": "SGE Senac - Atividades Extracurriculares",
+  "short_name": "SGE Senac",
+  "start_url": "./Login/index.html",
+  "scope": "./",
+  "display": "standalone",
+  "background_color": "#ff7a00",
+  "theme_color": "#003a8f",
+  "orientation": "portrait",
+  "icons": [
+    {
+      "src": "./icons/launchericon-72x72.png",
+      "sizes": "72x72",
+      "type": "image/png"
+    },
+    {
+      "src": "./icons/launchericon-96x96.png",
+      "sizes": "96x96",
+      "type": "image/png"
+    },
+    {
+      "src": "./icons/launchericon-144x144.png",
+      "sizes": "144x144",
+      "type": "image/png"
+    },
+    {
+      "src": "./icons/launchericon-192x192.png",
+      "sizes": "192x192",
+      "type": "image/png"
+    },
+    {
+      "src": "./icons/launchericon-512x512.png",
+      "sizes": "512x512",
+      "type": "image/png"
+    }
+  ]
+}
